01abf67369fdfc59f81a368cf285850e9a4db4b2
LouBingLin <loubinglin@lscm.com>
2026-05-06 17:20:26 +0800

    1、新增方法，用户能自定义输入消息的数据类型 2、新增系统提示词，在对话中引入提示词

--- src/test/java/com/example/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/example/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.aicodehelper;
 
+import com.example.aicodehelper.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiCodeHelperApplicationTests {
 
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
     @Test
-    void contextLoads() {
+    void chat() {
+        aiCodeHelper.chat("你好，我是程序员布朗尼");
     }
 
+    @Test
+    void chatWithMessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://ts3.tc.mm.bing.net/th/id/OIP-C.G6fkfQYRxcVmFwV_HX7bnQHaEH?r=0&rs=1&pid=ImgDetMain&o=7&rm=3")
+        );
+        aiCodeHelper.chatWithMessage(userMessage);
+    }
 }
